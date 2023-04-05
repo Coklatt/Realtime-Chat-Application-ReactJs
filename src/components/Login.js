fbb6b215d9747f83a3c5ac33482c731ed944e9ad
reader00 <qoryamanahputra0708@gmail.com>
2023-04-05 14:59:59 +0700

    fixing firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,40 +1,36 @@
-import React from "react";
-import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
-import firebase from "firebase/app";
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+import { GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from 'firebase/auth';
 
-import { auth } from "../firebase";
+import { auth } from '../firebase';
 
 const Login = () => {
-  return (
-    <div id="login-page">
-      <div id="login-card">
-        <h2> Welcome to MyChat! </h2>
-        <div
-          className="login-button google"
-          onClick={() =>
-            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-          }
-        >
-          <GoogleOutlined /> Sign In with Google
+    return (
+        <div id="login-page">
+            <div id="login-card">
+                <h2> Welcome to MyChat! </h2>
+                <div
+                    className="login-button google"
+                    onClick={() => signInWithPopup(auth, new GoogleAuthProvider())}
+                >
+                    <GoogleOutlined /> Sign In with Google
+                </div>
+                <br /> <br />
+                <div
+                    className="login-button facebook"
+                    onClick={() => signInWithPopup(auth, new FacebookAuthProvider())}
+                >
+                    <FacebookOutlined /> Sign In with Facebook
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <h4>Design by Alan Binu 💝</h4>
+            </div>
         </div>
-        <br /> <br />
-        <div
-          className="login-button facebook"
-          onClick={() =>
-            auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())
-          }
-        >
-          <FacebookOutlined /> Sign In with Facebook
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <h4>Design by Alan Binu 💝</h4>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,16 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-export const auth = firebase
-  .initializeApp({
-      // paste your firebase app config
-  })
-  .auth();
+const firebaseConfig = {
+    apiKey: 'AIzaSyDYj3NUHjlj8WKd2iLLDlvZGV-AkuxrpRY',
+    authDomain: 'realtime-chat-ta.firebaseapp.com',
+    projectId: 'realtime-chat-ta',
+    storageBucket: 'realtime-chat-ta.appspot.com',
+    messagingSenderId: '158600501859',
+    appId: '1:158600501859:web:a77dcd2ec7c2f59ebf3656',
+    measurementId: 'G-GLKGFP91ZQ',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
